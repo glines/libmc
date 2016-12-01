4d74c630252a0e46fda9dbec357bd1cd47223f1f
Jonathan Glines <jonathan@glines.net>
2016-11-30 23:44:06 -0700

    Working on zOrderNode neigbors. (WIP)

--- include/mc/common/zOrderNode.h
+++ include/mc/common/zOrderNode.h
@@ -33,6 +33,12 @@
 typedef struct { \
   int coord[DIMENSION]; \
 } mc ## PREFIX ## NodeCoordinates; \
+/* FIXME: mc ## PREFIX ## SpaceCoordinates could be replaced by
+ * mcVec ## DIMENSION, but we would need to address the issue that C++ does not
+ * support anonymous structs inside of unions. */ \
+typedef struct { \
+  float coord[DIMENSION]; \
+} mc ## PREFIX ## SpaceCoordinates; \
 typedef struct mc ## PREFIX ## Node mc ## PREFIX ## Node; \
 struct mc ## PREFIX ## Node { \
   mc ## PREFIX ## Node *children[1 << DIMENSION]; \
@@ -46,6 +52,12 @@ typedef struct { \
   mc ## PREFIX ## Node *current; \
 } mc ## PREFIX ## NodeIterator; \
 \
+typedef struct { \
+  mc ## PREFIX ## Node *node; \
+  int maskStack[sizeof(int) * 8]; \
+  int latticeIndex; \
+} mc ## PREFIX ## LatticeIterator; \
+\
 void mc ## PREFIX ## Node_init(mc ## PREFIX ## Node *self); \
 void mc ## PREFIX ## Node_initRoot(mc ## PREFIX ## Node *root); \
 void mc ## PREFIX ## Node_destroy(mc ## PREFIX ## Node *self); \
@@ -53,16 +65,30 @@ mc ## PREFIX ## NodeIterator mc ## PREFIX ## Node_begin( \
     mc ## PREFIX ## Node *self); \
 mc ## PREFIX ## NodeIterator mc ## PREFIX ## Node_end( \
     mc ## PREFIX ## Node *self); \
+mc ## PREFIX ## LatticeIterator mc ## PREFIX ## Node_beginLattice( \
+    mc ## PREFIX ## Node *self); \
+mc ## PREFIX ## LatticeIterator mc ## PREFIX ## Node_endLattice( \
+    mc ## PREFIX ## Node *self); \
 mc ## PREFIX ## Node *mc ## PREFIX ## Node_getNode( \
     mc ## PREFIX ## Node *self, \
     const mc ## PREFIX ## NodeCoordinates *pos, \
     int level); \
+int mc ## PREFIX ## Node_containsPoint( \
+    const mc ## PREFIX ## Node *self, \
+    const mc ## PREFIX ## SpaceCoordinates *point); \
+mc ## PREFIX ## Node *mc ## PREFIX ## Node_getNodeContainingPoint( \
+    const mc ## PREFIX ## Node *self, \
+    const mc ## PREFIX ## SpaceCoordinates *point); \
+mc ## PREFIX ## Node *mc ## PREFIX ## Node_makeNodeContainingPoint( \
+    mc ## PREFIX ## Node *self, \
+    const mc ## PREFIX ## SpaceCoordinates *point); \
 mc ## PREFIX ## Node *mc ## PREFIX ## Node_getRoot( \
     mc ## PREFIX ## Node *self); \
 int mc ## PREFIX ## Node_contains( \
     const mc ## PREFIX ## Node *self, \
     const mc ## PREFIX ## NodeCoordinates *pos, \
     int level); \
+/* FIXME: Need a routine for simply increasing the size of the root */ \
 void mc ## PREFIX ## Node_growRoot( \
     mc ## PREFIX ## Node *root, \
     const mc ## PREFIX ## NodeCoordinates *pos, \
@@ -88,7 +114,10 @@ mc ## PREFIX ## Node *mc ## PREFIX ## Node_createChild( \
 void mc ## PREFIX ## NodeIterator_next(mc ## PREFIX ## NodeIterator *self); \
 int mc ## PREFIX ## NodeIterator_equals( \
     const mc ## PREFIX ## NodeIterator *self, \
-    mc ## PREFIX ## NodeIterator other);
+    mc ## PREFIX ## NodeIterator other); \
+\
+void mc ## PREFIX ## LatticeIterator_next( \
+    mc ## PREFIX ## LatticeIterator *self);
 
 #define MC_DEFINE_Z_ORDER_NODE(PREFIX, DIMENSION) \
   MC_DEFINE_Z_ORDER_alignPosToLevel(PREFIX, DIMENSION) \
@@ -97,8 +126,13 @@ int mc ## PREFIX ## NodeIterator_equals( \
   MC_DEFINE_Z_ORDER_NODE_destroy(PREFIX, DIMENSION) \
   MC_DEFINE_Z_ORDER_NODE_begin(PREFIX, DIMENSION) \
   MC_DEFINE_Z_ORDER_NODE_end(PREFIX, DIMENSION) \
+  MC_DEFINE_Z_ORDER_NODE_beginLattice(PREFIX, DIMENSION) \
+  MC_DEFINE_Z_ORDER_NODE_endLattice(PREFIX, DIMENSION) \
   MC_DEFINE_Z_ORDER_NODE_nextSibling(PREFIX, DIMENSION) \
   MC_DEFINE_Z_ORDER_NODE_getNode(PREFIX, DIMENSION) \
+  MC_DEFINE_Z_ORDER_NODE_containsPoint(PREFIX, DIMENSION) \
+  MC_DEFINE_Z_ORDER_NODE_getNodeContainingPoint(PREFIX, DIMENSION) \
+  MC_DEFINE_Z_ORDER_NODE_makeNodeContainingPoint(PREFIX, DIMENSION) \
   MC_DEFINE_Z_ORDER_NODE_getRoot(PREFIX, DIMENSION) \
   MC_DEFINE_Z_ORDER_NODE_contains(PREFIX, DIMENSION) \
   MC_DEFINE_Z_ORDER_NODE_growRoot(PREFIX, DIMENSION) \
@@ -108,7 +142,8 @@ int mc ## PREFIX ## NodeIterator_equals( \
   MC_DEFINE_Z_ORDER_NODE_getChild(PREFIX, DIMENSION) \
   MC_DEFINE_Z_ORDER_NODE_createChild(PREFIX, DIMENSION) \
   MC_DEFINE_Z_ORDER_NODE_ITERATOR_next(PREFIX, DIMENSION) \
-  MC_DEFINE_Z_ORDER_NODE_ITERATOR_equals(PREFIX, DIMENSION)
+  MC_DEFINE_Z_ORDER_NODE_ITERATOR_equals(PREFIX, DIMENSION) \
+  MC_DEFINE_Z_ORDER_LATTICE_ITERATOR_next(PREFIX, DIMENSION)
 
 #define MC_DEFINE_Z_ORDER_alignPosToLevel(PREFIX, DIMENSION) \
   void mc ## PREFIX ## _alignPosToLevel( \
@@ -176,6 +211,28 @@ int mc ## PREFIX ## NodeIterator_equals( \
     return i; \
   }
 
+#define MC_DEFINE_Z_ORDER_NODE_beginLattice(PREFIX, DIMENSION) \
+  mc ## PREFIX ## LatticeIterator mc ## PREFIX ## Node_beginLattice( \
+      mc ## PREFIX ## Node *self) \
+  { \
+    mc ## PREFIX ## LatticeIterator i; \
+    i.node = self; \
+    memset(i.maskStack, 0, sizeof(i.maskStack)); \
+    i.latticeIndex = 0;  /* Start iterating over the lattice at the
+                            bottom-left corner of the node */ \
+  }
+
+#define MC_DEFINE_Z_ORDER_NODE_endLattice(PREFIX, DIMENSION) \
+  mc ## PREFIX ## LatticeIterator mc ## PREFIX ## Node_endLattice( \
+      mc ## PREFIX ## Node *self) \
+{ \
+  mc ## PREFIX ## LatticeIterator i; \
+  i.node = self; \
+  memset(i.maskStack, 0, sizeof(i.maskStack)); \
+  i.latticeIndex = 3;  /* Finish iterating over the lattice at the top-right
+                          corner of the node */ \
+}
+
 #define MC_DEFINE_Z_ORDER_NODE_nextSibling(PREFIX, DIMENSION) \
   mc ## PREFIX ## Node *mc ## PREFIX ## Node_nextSibling( \
       mc ## PREFIX ## Node *self) \
@@ -239,6 +296,68 @@ int mc ## PREFIX ## NodeIterator_equals( \
     assert(node); \
     return node; \
   }
+
+#define MC_DEFINE_Z_ORDER_NODE_containsPoint(PREFIX, DIMENSION) \
+  int mc ## PREFIX ## Node_containspoint( \
+      const mc ## PREFIX ## Node *self, \
+      const mc ## PREFIX ## SpaceCoordinates *point) \
+{ \
+  mc ## PREFIX ## NodeCoordinates pointPos; \
+  /* Quantize the point in space coordinates to node coordinates */ \
+  for (int i = 0; i < DIMENSION; ++i) { \
+    /* TODO: We might want to implement a more sophisticated mapping between
+     * coordinate systems here */ \
+    pointPos.coord[i] = (int)floor(point->coord[i]); \
+  } \
+  /* Check each dimension for containment by the node */ \
+  for (int i = 0; i < DIMENSION; ++i) { \
+    if (pointPos.coord[i] < self->pos.coord[i]) \
+      return 0; \
+    if (pointPos.coord[i] >= self->pos.coord[i] + (1 << self->level)) \
+      return 0; \
+  } \
+  return 1; \
+}
+
+#define MC_DEFINE_Z_ORDER_NODE_getNodeContainingPoint(PREFIX, DIMENSION) \
+  mc ## PREFIX ## Node *mc ## PREFIX ## Node_getNodeContainingPoint( \
+      const mc ## PREFIX ## Node *self, \
+      const mc ## PREFIX ## SpaceCoordinates *point) \
+{ \
+  /* Check if this node contains the given point */ \
+  if (!mc ## PREFIX ## Node_containsPoint(self, point)) { \
+    return NULL; \
+  } \
+  /* Iterate over children looking for child that contains this point */ \
+  for (int i = 0; (1 << DIMENSION); ++i) { \
+    mc ## PREFIX ## Node *containingNode; \
+    if (!self->children[i]) \
+      continue; \
+    containingNode = \
+      mc ## PREFIX ## Node_getNodeContainingPoint( \
+          self->children[i], point); \
+    if (containingNode != NULL) \
+      return containingNode; \
+  } \
+  /* Could not find any child containing the point; contained in this node */ \
+  return self; \
+}
+
+#define MC_DEFINE_Z_ORDER_NODE_makeNodeContainingPoint(PREFIX, DIMENSION) \
+  mc ## PREFIX ## Node *mc ## PREFIX ## Node_makeNodeContainingPoint( \
+      mc ## PREFIX ## Node *self, \
+      const mc ## PREFIX ## SpaceCoordinates *point) \
+{ \
+  mc ## PREFIX ## Node *root; \
+  /* TODO: Obtain a pointer to the root node */ \
+  root = mc ## PREFIX ## Node_getRoot(self); \
+  /* TODO: Check if the root node contains this point and grow if necessary */ \
+  while (!mc ## PREFIX ## Node_containsPoint(root, point)) { \
+    /* FIXME: We need a growRoot routine that does not require levels. */ \
+/*    mc ## PREFIX ## Node_growRoot(root); */ \
+  } \
+  /* TODO: Recursively find the containing node */ \
+}
 
 #define MC_DEFINE_Z_ORDER_NODE_getRoot(PREFIX, DIMENSION) \
   mc ## PREFIX ## Node *mc ## PREFIX ## Node_getRoot( \
@@ -467,6 +586,21 @@ void mc ## PREFIX ## Node_childIndexToPos( \
       mc ## PREFIX ## NodeIterator other) \
   { \
     return self->current == other.current; \
+  }
+
+#define MC_DEFINE_Z_ORDER_LATTICE_ITERATOR_next(PREFIX, DIMENSION) \
+  void mc ## PREFIX ## LatticeIterator_next( \
+      mc ## PREFIX ## LatticeIterator *self) \
+  { \
+    /* Each node has four potential lattice points associated with it. Up to
+     * three of these lattice points might also be associated with a parent
+     * node.
+     */ \
+    /* TODO: We need to traverse down the tree */ \
+    /* TODO: We need to make sure the very last lattice point we iterate over
+     * is index 3 on this node */ \
+    /* TODO: Assert that the lattice point that we are currently at is not
+     * divisible by the lattice level of our parent nodes */ \
   }
 
 #endif
